9d942f79b15887c33b11c23dde7b1924fd1f304a
vlauncher <v2dent@gmail.com>
2025-07-16 12:12:03 +0100

    changed cors

--- src/app.ts
+++ src/app.ts
@@ -20,17 +20,7 @@ const allowedOrigins = process.env.CORS_ORIGINS ? process.env.CORS_ORIGINS.split
 // app.use(helmet());
 
 // Use cors with allowed origins
-app.use(cors({
-  origin: function(origin, callback) {
-    // allow requests with no origin (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 app.use(express.json());
 
